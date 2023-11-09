c90f29fe1985561d195cac49c33774d2500a2d43
HunterLearns2Code <121377628+HunterLearns2Code@users.noreply.github.com>
2023-11-08 22:26:55 -0500

    Add files via upload

--- notepad.js
+++ notepad.js
@@ -1,11 +1,11 @@
-// JavaScript for the notepad page
+
 const notepadContent = document.getElementById('notepad-content');
 const saveButton = document.getElementById('save-button');
 
-// Load saved content from localStorage (if available)
+
 notepadContent.value = localStorage.getItem('notepadContent') || '';
 
-// Save content to localStorage when the "Save" button is clicked
+
 saveButton.addEventListener('click', function () {
     const content = notepadContent.value;
     localStorage.setItem('notepadContent', content);
